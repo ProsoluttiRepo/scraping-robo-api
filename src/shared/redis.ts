37567b9ac90f9867ba11828fddcf3a0d04052bed
rafaaelnicacio <rafaelnicacio154@gmail.com>
2025-08-22 17:59:55 -0300

    feat: atualiza configuração do Redis para usar host e porta padrão

--- src/shared/redis.ts
+++ src/shared/redis.ts
@@ -1,8 +1,8 @@
 import Redis from 'ioredis';
 
 const redis = new Redis({
-  host: 'localhost', // ou o host do container Redis
-  port: 6380,
+  host: 'redis', // ou o host do container Redis
+  port: 6379,
   // password: 'senha', se necessário
 });
 
