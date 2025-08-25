8a35339539c8142d12f1f7968cb56930f37fa6f4
rafaaelnicacio <rafaelnicacio154@gmail.com>
2025-08-25 10:30:04 -0300

    fix: corrige host e porta padrão do Redis para valores corretos

--- src/shared/redis.ts
+++ src/shared/redis.ts
@@ -1,8 +1,8 @@
 import Redis from 'ioredis';
 
 const redis = new Redis({
-  host: process.env.REDIS_HOST || 'localhost', // ou o host do container Redis
-  port: Number(process.env.REDIS_PORT) || 6380,
+  host: process.env.REDIS_HOST || 'redis', // ou o host do container Redis
+  port: Number(process.env.REDIS_PORT) || 6379,
   // password: 'senha', se necessário
 });
 
