c148b92bff9b6b3daea6cd1855ad7523668a5e2b
rafaaelnicacio <rafaelnicacio154@gmail.com>
2025-08-25 14:36:39 -0300

    refactor: habilita CORS para permitir requisições da API externa

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 8081;
+  app.enableCors({
+    origin: ['https://api.analisesprosolutti.com'],
+    credentials: true, // Permite o envio de cookies
+  });
   await app.listen(port);
 }
 bootstrap();
