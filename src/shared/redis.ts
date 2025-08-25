cb2875c9c31128d006514b13545fd4d1bdea8f7a
rafaaelnicacio <rafaelnicacio154@gmail.com>
2025-08-25 10:02:49 -0300

    feat: altera porta padrão do serviço para 8081 e ajusta configuração do Redis

--- src/shared/redis.ts
+++ src/shared/redis.ts
@@ -1,8 +1,8 @@
 import Redis from 'ioredis';
 
 const redis = new Redis({
-  host: 'redis', // ou o host do container Redis
-  port: 6379,
+  host: process.env.REDIS_HOST || 'localhost', // ou o host do container Redis
+  port: Number(process.env.REDIS_PORT) || 6380,
   // password: 'senha', se necessário
 });
 
