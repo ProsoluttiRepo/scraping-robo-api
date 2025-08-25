8724d7f3f51b7ff17a874401cca394fdcbf0bd02
rafaaelnicacio <rafaelnicacio154@gmail.com>
2025-08-25 11:03:50 -0300

    refactor: reorganiza a configuração do BullModule no app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
 import { Module } from '@nestjs/common';
 
-import { ConfigModule } from '@nestjs/config';
 import { PjeModule } from './modules/pje/pje.module';
+import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
 import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     PjeModule,
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST || 'redis',
+        port: Number(process.env.REDIS_PORT) || 6379,
+      },
+      limiter: { max: 2, duration: 1000 },
+      defaultJobOptions: {
+        removeOnComplete: 100,
+        removeOnFail: 1000,
+        attempts: 3,
+        backoff: {
+          type: 'exponential',
+          delay: 1000,
+        },
+      },
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
