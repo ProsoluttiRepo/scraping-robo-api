bc42f57e296f529ede777de457a78f6d9795f585
rafaaelnicacio <rafaelnicacio154@gmail.com>
2025-10-03 09:31:32 -0300

    fix: adiciona integração com Bull Board para monitoramento de filas em ambiente de desenvolvimento

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@bull-board/express';
+import { Queue } from 'bull';
+import { createBullBoard } from '@bull-board/api';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +12,16 @@ async function bootstrap() {
     origin: ['https://api.analisesprosolutti.com'],
     credentials: true, // Permite o envio de cookies
   });
+  if (process.env?.ENVIRONMENT !== 'production') {
+    const serverAdapter = new ExpressAdapter();
+    serverAdapter.setBasePath('/bull-board');
+    const aQueue = app.get<Queue>(`BullQueue_process-queue`);
+    createBullBoard({
+      queues: [new BullAdapter(aQueue)],
+      serverAdapter,
+    });
+    app.use('/bull-board', serverAdapter.getRouter());
+  }
   await app.listen(port);
 }
 bootstrap();
